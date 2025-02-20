36cea7470f4eef0c2bb3e91bac80d7c44d7e43cb
PatelPruthvi <pruthvi@mapsense.in>
2025-02-20 18:17:25 +0530

    initial commit
    setup for easy dictionary app

---
+++ lib/models/dictionary_model.dart
@@ -0,0 +1,168 @@
+import 'dart:convert';
+
+class DictionaryModel {
+  String word;
+  String phonetic;
+  List<Phonetic> phonetics;
+  List<Meaning> meanings;
+  License license;
+  List<String> sourceUrls;
+
+  DictionaryModel({
+    required this.word,
+    required this.phonetic,
+    required this.phonetics,
+    required this.meanings,
+    required this.license,
+    required this.sourceUrls,
+  });
+
+  factory DictionaryModel.fromRawJson(String str) =>
+      DictionaryModel.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory DictionaryModel.fromJson(Map<String, dynamic> json) =>
+      DictionaryModel(
+        word: json["word"],
+        phonetic: json["phonetic"],
+        phonetics: List<Phonetic>.from(
+            json["phonetics"].map((x) => Phonetic.fromJson(x))),
+        meanings: List<Meaning>.from(
+            json["meanings"].map((x) => Meaning.fromJson(x))),
+        license: License.fromJson(json["license"]),
+        sourceUrls: List<String>.from(json["sourceUrls"].map((x) => x)),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "word": word,
+        "phonetic": phonetic,
+        "phonetics": List<dynamic>.from(phonetics.map((x) => x.toJson())),
+        "meanings": List<dynamic>.from(meanings.map((x) => x.toJson())),
+        "license": license.toJson(),
+        "sourceUrls": List<dynamic>.from(sourceUrls.map((x) => x)),
+      };
+}
+
+class License {
+  String name;
+  String url;
+
+  License({
+    required this.name,
+    required this.url,
+  });
+
+  factory License.fromRawJson(String str) => License.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory License.fromJson(Map<String, dynamic> json) => License(
+        name: json["name"],
+        url: json["url"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "name": name,
+        "url": url,
+      };
+}
+
+class Meaning {
+  String partOfSpeech;
+  List<Definition> definitions;
+  List<String> synonyms;
+  List<String> antonyms;
+
+  Meaning({
+    required this.partOfSpeech,
+    required this.definitions,
+    required this.synonyms,
+    required this.antonyms,
+  });
+
+  factory Meaning.fromRawJson(String str) => Meaning.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory Meaning.fromJson(Map<String, dynamic> json) => Meaning(
+        partOfSpeech: json["partOfSpeech"],
+        definitions: List<Definition>.from(
+            json["definitions"].map((x) => Definition.fromJson(x))),
+        synonyms: List<String>.from(json["synonyms"].map((x) => x)),
+        antonyms: List<String>.from(json["antonyms"].map((x) => x)),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "partOfSpeech": partOfSpeech,
+        "definitions": List<dynamic>.from(definitions.map((x) => x.toJson())),
+        "synonyms": List<dynamic>.from(synonyms.map((x) => x)),
+        "antonyms": List<dynamic>.from(antonyms.map((x) => x)),
+      };
+}
+
+class Definition {
+  String definition;
+  List<dynamic> synonyms;
+  List<dynamic> antonyms;
+  String example;
+
+  Definition({
+    required this.definition,
+    required this.synonyms,
+    required this.antonyms,
+    required this.example,
+  });
+
+  factory Definition.fromRawJson(String str) =>
+      Definition.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory Definition.fromJson(Map<String, dynamic> json) => Definition(
+        definition: json["definition"],
+        synonyms: List<dynamic>.from(json["synonyms"].map((x) => x)),
+        antonyms: List<dynamic>.from(json["antonyms"].map((x) => x)),
+        example: json["example"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "definition": definition,
+        "synonyms": List<dynamic>.from(synonyms.map((x) => x)),
+        "antonyms": List<dynamic>.from(antonyms.map((x) => x)),
+        "example": example,
+      };
+}
+
+class Phonetic {
+  String text;
+  String audio;
+  String sourceUrl;
+  License license;
+
+  Phonetic({
+    required this.text,
+    required this.audio,
+    required this.sourceUrl,
+    required this.license,
+  });
+
+  factory Phonetic.fromRawJson(String str) =>
+      Phonetic.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory Phonetic.fromJson(Map<String, dynamic> json) => Phonetic(
+        text: json["text"],
+        audio: json["audio"],
+        sourceUrl: json["sourceUrl"],
+        license: License.fromJson(json["license"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "text": text,
+        "audio": audio,
+        "sourceUrl": sourceUrl,
+        "license": license.toJson(),
+      };
+}
