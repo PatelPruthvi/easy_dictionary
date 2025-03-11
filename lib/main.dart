65e7ebbe0ae9938cc1a91e8b07e65f12b5db04b7
PatelPruthvi <pruthvi@mapsense.in>
2025-03-11 17:57:44 +0530

    Integrated firebase with mobile app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:easy_dictionary/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'views/home_view/home_view.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
