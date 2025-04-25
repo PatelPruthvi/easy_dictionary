a3cd9dbb7e3fd35f3ec10e4cb7aada6c5df91d7f
PatelPruthvi <pruthvi@mapsense.in>
2025-04-25 11:43:11 +0530

    implemented potrait lock

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:easy_dictionary/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'views/home_view/home_view.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
 
